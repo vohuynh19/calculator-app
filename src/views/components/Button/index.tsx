dfa5264b92afff1c8da72dde6d3c7109acd5d6cf
vhuynh192k2 <huynh.vo@digitalts.co>
2022-09-29 17:35:35 +0700

    Add box-shadow on hover and onclick

--- src/views/components/Button/index.tsx
+++ src/views/components/Button/index.tsx
@@ -1,12 +1,32 @@
 import { ButtonProps } from "./types";
 import "./index.css";
+import { useRef } from "react";
 
 const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
+  const ref = useRef<any>(null);
+
+  const _handleShowShadowEffect = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e = e || window.event;
+    e.preventDefault();
+
+    ref.current.classList.add("highlight");
+
+    setTimeout(() => {
+      ref.current.classList.remove("highlight");
+    }, 300);
+  };
+
   return (
     <button
       {...props}
+      ref={ref}
       className={`btn ${props.className}`}
-      onClick={props.onClick}
+      onClick={(event) => {
+        _handleShowShadowEffect(event);
+        props?.onClick?.(event);
+      }}
     >
       {children}
     </button>
